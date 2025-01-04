263123f44d84983262b44adf01db36d4a8e02085
Pedro César <pedrocdefreitas@gmail.com>
2025-01-04 18:10:46 -0300

    Atualização da função de saque concluida

--- app.mjs
+++ app.mjs
@@ -1,166 +1,211 @@
-"use strict";
+let historicoOperacoes = [];
+let SaldoInicial = 100
 
-const xhr = new XMLHttpRequest();
-let valorSalvoHistorico;
-let historicoSaqueDB = [];
-let itensDoMenu = document.querySelectorAll("[data-menu-main]");
+let capturaLinksMenuPrincipal = function () {
+  //Função adicionando evento de click nos botões e retornando o link para suas respectivas.
+  let itensDoMenu = document.querySelectorAll("[data-menu-main]");
 
-let menu = {
-  saque: {
-    idName: "saque",
-    linkMenu: "./assets/src/views/Sacar.html",
-  },
+  let menu = {
+    saque: {
+      idName: "saque",
+      linkMenu: "./assets/src/views/Sacar.html",
+    },
 
-  deposito: {
-    idName: "deposito",
-    linkMenu: "./assets/src/views/Depositar.html",
-  },
+    deposito: {
+      idName: "deposito",
+      linkMenu: "./assets/src/views/Depositar.html",
+    },
 
-  pix: {
-    idName: "pix",
-    linkMenu: "./assets/src/views/Pix.html",
-  },
+    pix: {
+      idName: "pix",
+      linkMenu: "./assets/src/views/Pix.html",
+    },
 
-  pagar: {
-    idName: "pagar",
-    linkMenu: "./assets/src/views/Pagamentos.html",
-  },
+    pagar: {
+      idName: "pagar",
+      linkMenu: "./assets/src/views/Pagamentos.html",
+    },
 
-  extrato: {
-    idName: "extrato",
-    linkMenu: "./assets/src/views/Extrato.html",
-  },
+    extrato: {
+      idName: "extrato",
+      linkMenu: "./assets/src/views/Extrato.html",
+    },
 
-  mais: {
-    idName: "mais",
-    linkMenu: "*",
-  },
-};
+    mais: {
+      idName: "mais",
+      linkMenu: "*",
+    },
+  };
 
-itensDoMenu.forEach((res) => {
-  res.addEventListener("click", (el) => {
-    const attr = el.target.attributes[0].value;
+  itensDoMenu.forEach((res) => {
+    res.addEventListener("click", (el) => {
+      const attr = el.target.attributes[0].value;
 
-    let resultado = Object.values(menu).forEach((obj) => {
-      if (obj.idName === attr) {
-        let link = obj.linkMenu;
-        // console.log(link)
-      }
+      let resultado = Object.values(menu).forEach((obj) => {
+        if (obj.idName === attr) {
+          let link = obj.linkMenu;
+          htmlRequest(link);
+        }
+      });
     });
   });
-});
-
-// itensDoMenu.forEach( div => {
-
-//     div.addEventListener('click', (elemento)=>{
-//         if( elemento.target.tagName === "DIV"){
-
-//             const attr = elemento.target.attributes[0].value
-
-//             let resultado = Object.values(menu).forEach( el =>{
-//                 if( attr === el.idName){ console.log( el.linkMenu )}
-//             })
-//         }
-
-//     })
-// })
-
-function setDataHistorico() {
-  let data = new Date();
-  let dataFormatada = `${data.getDate()} ${
-    data.getMonth() + 1
-  } ${data.getFullYear()}`;
-  return dataFormatada;
-}
-
-function setHoraHistorico() {
-  let data = new Date();
-  let HoraFormatada = `${data.getHours()} ${data.getMinutes()} ${data.getSeconds()}`;
-  return HoraFormatada;
-}
-
-xhr.open("GET", "./assets/src/views/Sacar.html", true);
-
-xhr.responseType = "text";
-
-xhr.onload = function () {
-  if (xhr.status === 200) {
-    let botaoDeSaque = document.querySelector('[data-menu-main="saque"]');
-    botaoDeSaque.addEventListener("click", function () {
-      let container = document.querySelector("#container-modal-main");
-      container.innerHTML = xhr.responseText;
-
-      let mainModal = document.querySelector("#main-modal");
-
-      setTimeout(function () {
-        mainModal.style.top = "0";
-      }, 100);
-
-      let botaoDesistir = document
-        .querySelector(".btn-de-desistir-tela-um")
-        .addEventListener("click", function () {
-          let confirma_is = confirm("Deseja desistir?");
-          if (confirma_is == true) {
-            window.location = "./paginaInicialPerfil.html";
-          }
-        });
-
-      let botaoConfirmaSaque = document
-        .querySelector("#btn-de-confirmar")
-        .addEventListener("click", function () {
-          // Validações do input
-          let valorInputSaque = document.querySelector("#valor-a-enviar");
-          if (valorInputSaque.value == "") {
-            throw new Error("Digite um valor para seguir com o saque");
-          }
-          if (isNaN(valorInputSaque.value)) {
-            throw new Error("Só é aceito números para seguir.");
-          }
-          valorSalvoHistorico = valorInputSaque.value;
-
-          // Verificação de confirmação do usuario para proseguir como saque
-          let confirmaAcao = confirm("Deseja confirmar saque?");
-
-          // Menssagem de aguarde, antes de realizar o saque.
-          let btnConfirma = document.querySelector("#btn-de-confirmar");
-          btnConfirma.textContent = "Aguarde a solicitação";
-
-          // Desição sobre a confirmação do usuario, se "ok" o saque é realizado
-          if (confirmaAcao == true) {
-            let salvaHistoricoDeSaque = function (
-              idCliente,
-              Op = "Debito",
-              Valor,
-              Data,
-              Hora
-            ) {
-              this.idCliente = idCliente;
-              this.operacao = Op;
-              this.valorDoSaque = Valor;
-              this.DataDoSaque = Data;
-              this.HoraDoSaque = Hora;
-            };
-
-            historicoSaqueDB.push(
-              new salvaHistoricoDeSaque(
-                "12345",
-                "Deb",
-                valorSalvoHistorico,
-                setDataHistorico(),
-                setHoraHistorico()
-              )
-            );
-
-            setTimeout(() => {
-              alert(
-                `Saque realizado com sucesso no valor de ${valorSalvoHistorico}`
-              );
-              console.log(historicoSaqueDB);
-            }, 3500);
-          }
-        });
-    });
-  }
 };
 
-xhr.send();
+let htmlRequest = function (linkPaginaMenu) {
+  //funcão para fazer a requisição das paginas dos links do menu.
+  let containerMenu = document.querySelector("#container-modal-menu");
+  let http = new XMLHttpRequest();
+  http.open("GET", linkPaginaMenu, true);
+  http.responseType = "text";
+
+  http.onload = function () {
+    if (http.status == 200 || http.status == 304) {
+      containerMenu.innerHTML = http.responseText;
+      funcaoSaque()
+      
+      
+    //É necessário esse tempo para que o elemento surga na tela, para então ocorrer a animação de descer.
+      setTimeout( function () {  
+        containerMenu.style.top = '0px'
+      }, 100)
+
+      acaoDoBotaoDesistir(containerMenu)
+    }
+
+  };
+
+  http.send();
+
+};
+
+let acaoDoBotaoDesistir = function( containerMenu ){
+  //Função para o botão de desistir ( está funcionando em todas as Views... )
+  let botaoDesistir = document.querySelector('.btn-de-desistir-tela-um')
+  botaoDesistir.addEventListener('click', function(){
+    console.log('desisti')
+
+    //É necessário esse tempo para que o elemento surga na tela, para então ocorrer a animação de subir.
+    setTimeout( function () {  
+      containerMenu.style.top = '-100vh'
+    }, 100)
+  })
+}
+
+let saudacaoInicial = function(){
+  let textoDataPorExtenso = document.querySelector('[data-perfil="data-sistema"]')
+  let now = new Date
+  let dayName = new Array ("Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado")
+  let monName = new Array ("janeiro", "fevereiro", "março", "abril", "maio", "junho", "agosto", "outubro", "novembro", "dezembro")
+  textoDataPorExtenso.textContent = "" + dayName[now.getDay() ] + ", " + now.getDate () + " de " + monName [now.getMonth() ]   +  " de "  +     now.getFullYear () + "."
+}
+
+
+let funcaoSaque = function(){
+
+    let elementosSaque = document.querySelectorAll('[data-modal-sacar]')
+    elementosSaque[3].addEventListener('keyup', function () {
+    let valorDigitado = elementosSaque[1].value 
+    let saldoAnterior = SaldoInicial
+
+    if(isNaN( valorDigitado )){ 
+      historicoOperacoes.push("Tentativa de saque não realizada, valor incorreto.")
+      throw new Error('Digite o valor corretamente.')}
+
+    if( SaldoInicial < valorDigitado){ 
+      historicoOperacoes.push("Tentativa de saque não realizada, Saldo insuficiente!")
+      throw new Error('Saldo insuficiente..' + SaldoInicial)}
+
+    let saldofinal = SaldoInicial -= valorDigitado
+
+    let dadosDoSaque = {
+      "idClinte": '123456',
+      "tipo": "saque",
+      "modo": "débito",
+      "taxAdm": '',
+      "saldoAnterior": saldoAnterior,
+      "valorSolicitado": valorDigitado,
+      "dataOperacao": '04/01/2025',
+      "descricao": 'Saque efetuado ATM',
+      "saldoAtualizado": saldofinal
+    }
+
+    historicoOperacoes.push(dadosDoSaque)
+    console.log( historicoOperacoes )
+
+  })
+
+}
+
+let funcaoDeposito = function(){
+  
+}
+
+
+
+//Inicio das funções - NÃO APAGUE.
+saudacaoInicial()
+capturaLinksMenuPrincipal();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function OperacaoBancaria(SaldoInicial, idCliente, DataOperacao, TipoOperacao, Descricao, Tarifa, Destinatario, ValorBruto, ValorNominal ){ 
+
+//     let saldoInicial = SaldoInicial
+//     //Cadastro base para operações bancárias.
+
+//     this.IdCliente    = idCliente
+//     this.DataOperacao = DataOperacao
+//     this.TipoOperacao = TipoOperacao
+//     this.Descricao    = Descricao
+//     this.Tarifa       = Tarifa
+//     this.Destinatario = Destinatario
+//     this.ValorBruto   = ValorBruto
+//     this.ValorNominal = ValorNominal
+
+
+//     this.Saque = function (ValorDoSaque, taxaDeSaque) {
+
+//       if( saldoInicial > ValorDoSaque) {
+//         return saldoInicial -= saldoInicial - ValorDoSaque
+//       }
+
+//       throw new Error('Sem saldo suficiente, Realize um deposito')
+//       //Ofereça o link para a tela do deposito.
+//     }
+
+//     // this.ResumoCliente = function(){}
+//     // this.Pix = function (ValorDoPix){
+//     //   if( SaldoInicial > ValorDoPix){
+//     //     return SaldoInicial -= ValorDoPix
+//     //   } 
+//     //   throw new Error('Sem saldo suficiente, Realize um deposito')
+//     // }
+
+//     // this.Deposito = function (ValorDeposito) {
+//     //   return SaldoInicial += ValorDeposito
+//     // }
+
+//     // this.Pagamento = function(){}
+
+//     // this.Extrato = function () {}
+
+//     // this.AtualizaSaldo = function( ){}
+
+//   }
