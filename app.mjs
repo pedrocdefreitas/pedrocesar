4075e404106de25ed25c04c84c9be0b51ecc1f2b
Pedro César <pedrocdefreitas@gmail.com>
2025-01-06 19:37:23 -0300

    todas as funções de operações bancarias já estão definidas

--- app.mjs
+++ app.mjs
@@ -1,5 +1,10 @@
-let historicoOperacoes = [];
-let SaldoInicial = 100
+let historicoDeposito = [];
+let SaldoInicial;
+
+// window.addEventListener("DOMContentLoaded", function (event) {
+//     return true
+// });
+
 
 let capturaLinksMenuPrincipal = function () {
   //Função adicionando evento de click nos botões e retornando o link para suas respectivas.
@@ -44,36 +49,40 @@ let capturaLinksMenuPrincipal = function () {
       let resultado = Object.values(menu).forEach((obj) => {
         if (obj.idName === attr) {
           let link = obj.linkMenu;
-          htmlRequest(link);
+          let idName = obj.idName
+          htmlRequest(link, idName);
         }
       });
     });
   });
 };
 
-let htmlRequest = function (linkPaginaMenu) {
+let htmlRequest = function (linkPaginaMenu, idName) {
   //funcão para fazer a requisição das paginas dos links do menu.
   let containerMenu = document.querySelector("#container-modal-menu");
   let http = new XMLHttpRequest();
+  
   http.open("GET", linkPaginaMenu, true);
   http.responseType = "text";
 
   http.onload = function () {
+
     if (http.status == 200 || http.status == 304) {
       containerMenu.innerHTML = http.responseText;
-      funcaoSaque()
-      
-      
-    //É necessário esse tempo para que o elemento surga na tela, para então ocorrer a animação de descer.
+
+      if(idName === 'saque')    funcaoSaque()
+      if(idName === 'deposito') funcaoDeposito()
+      if(idName === 'pix')      funcaoPix()
+      if(idName === 'pagar') funcaoPagamento()
+      //É necessário esse tempo para que o elemento surga na tela, para então ocorrer a animação de descer.
       setTimeout( function () {  
         containerMenu.style.top = '0px'
       }, 100)
-
       acaoDoBotaoDesistir(containerMenu)
     }
-
   };
 
+  
   http.send();
 
 };
@@ -98,7 +107,6 @@ let saudacaoInicial = function(){
   let monName = new Array ("janeiro", "fevereiro", "março", "abril", "maio", "junho", "agosto", "outubro", "novembro", "dezembro")
   textoDataPorExtenso.textContent = "" + dayName[now.getDay() ] + ", " + now.getDate () + " de " + monName [now.getMonth() ]   +  " de "  +     now.getFullYear () + "."
 }
-
 
 let funcaoSaque = function(){
 
@@ -137,12 +145,132 @@ let funcaoSaque = function(){
 }
 
 let funcaoDeposito = function(){
+    let elemento = document.querySelectorAll('[data-modal-deposito]')
+    let saldoAnterior = SaldoInicial
+
+    elemento[2].addEventListener('click', function(){
+
+      let valorParaDepositar = parseFloat(elemento[0].value)
+
+        if(isNaN( valorParaDepositar )){ 
+            throw new Error('Digite o valor corretamente.')}
+
+       let deposito = SaldoInicial += valorParaDepositar
+
+       let dadosDoDeposito = {
+        "idClinte": '123456',
+        "tipo": "deposito",
+        "modo": "crédito",
+        "taxAdm": '',
+        "valorDepositado": valorParaDepositar,
+        "dataOperacao": '04/01/2025',
+        "descricao": 'Deposito efetuado no ATM',
+      }
+
+      historicoDeposito.push(dadosDoDeposito)
+      localStorage.setItem('dados_do_deposito', JSON.stringify(historicoDeposito));
+      console.log('salvo no localstorage.')
+  })
   
 }
 
+let funcaoPix = function(){
 
+  let elementosPix = document.querySelectorAll('[data-modal-pix]')
+  let tipoPix;
+  let botaoConfirma = elementosPix[3]
+  let pixDestinatario = elementosPix[0]
+
+  pixDestinatario.addEventListener('keyup', function(){
+
+
+    if(pixDestinatario.value.includes('@')){
+     console.log('Pix do tipo email.')
+     tipoPix = "email."
+    }
+
+    console.log(pixDestinatario.value.length)
+    
+    if(pixDestinatario.value.length == '12'){
+     console.log('Pix do tipo celular.')
+     tipoPix = 'celular'
+    }
+
+    if(pixDestinatario.value.length == '14'){
+      console.log('Pix do tipo cnpj.')
+      tipoPix = 'cnpj'
+    }
+
+    if(pixDestinatario.value.length == '11'){
+      console.log('Pix do tipo cpf.')
+      tipoPix = 'cpf'
+    }
+
+  })  
+
+
+  botaoConfirma.addEventListener('click', function () {  
+
+    if(pixDestinatario.value == ""){
+      console.log('O campo de remetente não pode ser em branco ')
+      return
+    }
+
+    if(elementosPix[1].value == ""){
+      console.log('O campo de valor a enviar, não pode ser em branco ')
+      return
+    }
+
+    let dadosPix = {
+      "idClinte": '123456',
+      'destinatario': pixDestinatario.value,
+      'tipo de pix': tipoPix || "tipo não encontrado",
+      'remetente': 'Pedro',
+      "valor": elementosPix[1].value,
+      "data": new Date(),
+      "descrição": "Envio",
+      "tipo": 'pix',
+      "tipo operacao": 'debito'
+    }
+
+    console.log( dadosPix )    
+  })
+
+}
+
+let funcaoPagamento = function(){
+  let elementosPagamentos = document.querySelectorAll('[data-modal-pagamento]')
+
+  let dadosDoCodigoDeBarras = {
+    "idClinte": '123456',
+    'beneficiario': "Companhia de energia do estado de pernambuco",
+    'data-documento': '25/12/2024',
+    'n-do-documento': 123456785,
+    'vencimento': '12/12/2024',
+    'data-do-pagamento': new Date()
+  }
+  
+  elementosPagamentos[4].addEventListener('click', function(){
+    let codBarras = elementosPagamentos[0].value
+    let vencimentoBoleto = elementosPagamentos[2].value
+    let valorBoleto = elementosPagamentos[1].value
+
+    let dadosBoleto = {
+      "idClinte": '123456',
+      'codigo-de-barras': codBarras,
+      'pagamento-data-boleto': vencimentoBoleto,
+      'valor-do-boleto': valorBoleto,
+
+        dadosDoBoleto: dadosDoCodigoDeBarras
+    }
+
+    console.log(dadosBoleto)
+  })
+
+}
 
 //Inicio das funções - NÃO APAGUE.
+
 saudacaoInicial()
 capturaLinksMenuPrincipal();
 
